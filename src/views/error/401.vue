f28b79af7abbfcc63a184012c49ba85126d07205
ranfenghua <ranfenghua@cecinvestment.com>
2024-11-12 09:29:14 +0800

    fix:优化提示

--- src/views/error/401.vue
+++ src/views/error/401.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="errPage-container">
     <el-button icon="arrow-left" class="pan-back-btn" @click="back">
-      {{ Common.GoBack }}
+      返回
     </el-button>
     <el-row>
       <el-col :span="12">
@@ -19,70 +19,86 @@
         </ul>
       </el-col>
       <el-col :span="12">
-        <img :src="errGif" width="313" height="428" alt="Girl has dropped her ice cream.">
+        <img
+          :src="errGif"
+          width="313"
+          height="428"
+          alt="Girl has dropped her ice cream."
+        />
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import errGif from '@/assets/401_images/401.gif'
+import errGif from "@/assets/401_images/401.gif";
 
 export default {
-  name: 'Page401',
+  name: "Page401",
   data() {
     return {
-      errGif: errGif + '?' + +new Date()
-    }
+      errGif: errGif + "?" + +new Date(),
+      Error: {
+        Err401: "401 Error!",
+        NoPermission: "Permission denied!",
+        NoPermissionTip:
+          "Sorry, you do not have access rights. Please do not engage in illegal operations! You can return to the main page.",
+        GoHome: "Back Home",
+        Err404: "404 Error!",
+        Err404Tip:
+          "Sorry, the page you are looking for does not exist. Try checking for errors in the URL, then press the refresh button on the browser or try to find other content in our application.",
+        PageNotFound: "Page not found!",
+      },
+    };
   },
   methods: {
     back() {
       if (this.$route.query.noGoBack) {
-        this.$router.push({ path: '/' })
+        this.$router.push({ path: "/" });
       } else {
-        this.$router.go(-1)
+        this.$router.go(-1);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.errPage-container {
+  width: 800px;
+  max-width: 100%;
+  margin: 100px auto;
+  .pan-back-btn {
+    background: #008489;
+    color: #fff;
+    border: none !important;
+  }
+  .pan-gif {
+    margin: 0 auto;
+    display: block;
+  }
+  .pan-img {
+    display: block;
+    margin: 0 auto;
+    width: 100%;
+  }
+  .text-jumbo {
+    font-size: 60px;
+    font-weight: 700;
+    color: #484848;
+  }
+  .list-unstyled {
+    font-size: 14px;
+    li {
+      padding-bottom: 5px;
+    }
+    a {
+      color: #008489;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
       }
     }
   }
 }
-</script>
-
-<style lang="scss" scoped>
-  .errPage-container {
-    width: 800px;
-    max-width: 100%;
-    margin: 100px auto;
-    .pan-back-btn {
-      background: #008489;
-      color: #fff;
-      border: none!important;
-    }
-    .pan-gif {
-      margin: 0 auto;
-      display: block;
-    }
-    .pan-img {
-      display: block;
-      margin: 0 auto;
-      width: 100%;
-    }
-    .text-jumbo {
-      font-size: 60px;
-      font-weight: 700;
-      color: #484848;
-    }
-    .list-unstyled {
-      font-size: 14px;
-      li {
-        padding-bottom: 5px;
-      }
-      a {
-        color: #008489;
-        text-decoration: none;
-        &:hover {
-          text-decoration: underline;
-        }
-      }
-    }
-  }
 </style>
--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -2,10 +2,26 @@
   <div class="wscn-http404-container">
     <div class="wscn-http404">
       <div class="pic-404">
-        <img class="pic-404__parent" src="@/assets/404_images/404.png" alt="404">
-        <img class="pic-404__child left" src="@/assets/404_images/404_cloud.png" alt="404">
-        <img class="pic-404__child mid" src="@/assets/404_images/404_cloud.png" alt="404">
-        <img class="pic-404__child right" src="@/assets/404_images/404_cloud.png" alt="404">
+        <img
+          class="pic-404__parent"
+          src="@/assets/404_images/404.png"
+          alt="404"
+        />
+        <img
+          class="pic-404__child left"
+          src="@/assets/404_images/404_cloud.png"
+          alt="404"
+        />
+        <img
+          class="pic-404__child mid"
+          src="@/assets/404_images/404_cloud.png"
+          alt="404"
+        />
+        <img
+          class="pic-404__child right"
+          src="@/assets/404_images/404_cloud.png"
+          alt="404"
+        />
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">
@@ -26,20 +42,28 @@
 </template>
 
 <script>
-
 export default {
-  name: 'Page404',
+  name: "Page404",
+  data() {
+    return {
+      Error: {
+        Err404: "404",
+        Err404Tip: "Sorry, the page you visited does not exist.",
+        GoHome: "Back to home",
+      },
+    };
+  },
   computed: {
     message() {
-      return this.$t('Error.PageNotFound')
-    }
-  }
-}
+      return this.$t("Error.PageNotFound");
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.wscn-http404-container{
-  transform: translate(-50%,-50%);
+.wscn-http404-container {
+  transform: translate(-50%, -50%);
   position: absolute;
   top: 40%;
   left: 50%;
