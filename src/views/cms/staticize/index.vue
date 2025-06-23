5b771e25119ef2a9e5e08a3e38e033c4003b5d65
ranfenghua <ranfenghua@cecinvestment.com>
2025-06-23 16:15:11 +0800

    feat(cms): 添加广告管理功能
    - 新增广告空间管理页面和相关 API
    - 实现广告投放和上下架功能
    - 添加广告统计页面，包括点击率和曝光率统计
    - 新增广告点击日志和曝光日志查看功能

--- src/views/cms/staticize/index.vue
+++ src/views/cms/staticize/index.vue
@@ -1,44 +1,104 @@
+
 <template>
   <div class="app-container">
-    <el-tabs v-model="activeName">
-      <el-tab-pane :label="$t('CMS.Staticize.Tag')" name="tag">
-        <cms-template-tag></cms-template-tag>
-      </el-tab-pane>
-      <el-tab-pane :label="$t('CMS.Staticize.Func')" name="function">
-        <cms-template-function></cms-template-function>
-      </el-tab-pane>
-      <el-tab-pane :label="$t('CMS.Staticize.DynamicTemplate')" name="dynamic">
-        <cms-dynamic-template></cms-dynamic-template>
-      </el-tab-pane>
-      <el-tab-pane :label="$t('CMS.Staticize.CustomDynamicTemplate')" name="customDynamic">
-        <cms-custom-dynamic-template></cms-custom-dynamic-template>
-      </el-tab-pane>
-    </el-tabs>
+    <el-row :gutter="20">
+      <el-col :span="4" :xs="24">
+        <div class="head-container">
+          <el-input
+            v-model="statName"
+            clearable
+            size="small"
+            prefix-icon="el-icon-search"
+            style="margin-bottom: 20px"
+          />
+        </div>
+        <div class="head-container">
+          <el-tree
+            :data="statMenuTree"
+            :props="defaultProps"
+            :expand-on-click-node="false"
+            :filter-node-method="filterNode"
+            ref="tree"
+            node-key="type"
+            default-expand-all
+            highlight-current
+            @node-click="handleNodeClick"
+          />
+        </div>
+      </el-col>
+      <el-col :span="20" :xs="24">
+        <cms-ad-stat v-if="currentMenu=='CmsAdStat'"></cms-ad-stat>
+        <cms-ad-click v-if="currentMenu=='CmsAdClickLog'"></cms-ad-click>
+        <cms-ad-view v-if="currentMenu=='CmsAdViewLog'"></cms-ad-view>
+        <cms-site-trend-overview v-if="currentMenu=='BdSiteTrendOverview'"></cms-site-trend-overview>
+        <cms-site-timetrend v-if="currentMenu=='BdSiteTimeTrend'"></cms-site-timetrend>
+        <cms-content-stat v-if="currentMenu=='ContentDynamicStat'"></cms-content-stat>
+        <cms-content-stat-by-catalog v-if="currentMenu=='ContentStatByCatalog'"></cms-content-stat-by-catalog>
+        <cms-content-stat-by-user v-if="currentMenu=='ContentStatByUser'"></cms-content-stat-by-user>
+      </el-col>
+    </el-row>
   </div>
 </template>
+
 <script>
-import CMSTemplateTag from '@/views/cms/staticize/tagList';
-import CMSTemplateFunc from '@/views/cms/staticize/funcList';
-import CMSDynamicTemplate from '@/views/cms/staticize/dynamicList';
-import CMSCustomDynamicTemplate from '@/views/cms/staticize/customDynamicList';
+import { getStatMenuTreeData } from "@/api/stat/stat";
+import CMSAdvertisementStat from '@/views/cms/ad/adStat';
+import CMSAdvertisementClickLog from '@/views/cms/ad/adClickLog';
+import CMSAdvertisementViewLog from '@/views/cms/ad/adViewLog';
+import CMSBdSiteTrendOverview from '@/views/cms/stat/bdSiteStat';
+import CMSBdSiteTimeTrend from '@/views/cms/stat/bdSiteTimeTrendStat';
+import CMSContentDynamicStat from '@/views/cms/stat/contentDynamicStat';
+import ContentStatByCatalog from '@/views/cms/stat/contentStatByCatalog';
+import ContentStatByUser from '@/views/cms/stat/contentStatByUser';
+
+
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
 
 export default {
-  name: "CMSStaticize",
-  components: {
-    'cms-template-tag': CMSTemplateTag,
-    'cms-template-function': CMSTemplateFunc,
-    'cms-dynamic-template': CMSDynamicTemplate,
-    'cms-custom-dynamic-template': CMSCustomDynamicTemplate,
+  name: "StatIndex",
+  components: { 
+    Treeselect,
+    'cms-ad-stat': CMSAdvertisementStat,
+    'cms-ad-click': CMSAdvertisementClickLog,
+    'cms-ad-view': CMSAdvertisementViewLog,
+    'cms-site-trend-overview': CMSBdSiteTrendOverview,
+    'cms-site-timetrend': CMSBdSiteTimeTrend,
+    'cms-content-stat': CMSContentDynamicStat,
+    'cms-content-stat-by-catalog': ContentStatByCatalog,
+    'cms-content-stat-by-user': ContentStatByUser
   },
-  data () {
+  data() {
     return {
-      activeName: 'tag'
+      loading: true,
+      statMenuTree: [],
+      currentMenu: undefined,
+      statName: undefined,
+      defaultProps: {}
     };
+  },
+  watch: {
+    statName(val) {
+      this.$refs.tree.filter(val);
+    }
+  },
+  created() {
+    this.getStatType();
+  },
+  methods: {
+    getStatType() {
+      getStatMenuTreeData().then(response => {
+        this.statMenuTree = response.data.treeData;
+        this.currentMenu = response.data.defaultMenu
+      });
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    },
+    handleNodeClick(data) {
+      this.currentMenu = data.id;
+    }
   }
 };
 </script>
-<style scoped>
-.el-tabs__header {
-  margin-bottom: 10px;
-}
-</style>
